5e5c9b667d06eb2609bdedc971fcdbd689490699
Oscar <oscar.bustamante01@usc.edu.co>
2017-10-06 10:27:02 -0500

    se adiciona el archivo de configuracion para el Mybaties

--- src/co/edu/usc/vision/interacciones/api/fileRider/test.java
+++ src/co/edu/usc/vision/interacciones/api/fileRider/test.java
@@ -1,26 +1,68 @@
 package co.edu.usc.vision.interacciones.api.fileRider;
 
-import co.edu.usc.vision.interacciones.api.dao.model.DrugbankMapper;
-import co.edu.usc.vision.interacciones.utiles.BdUtils;
-import org.apache.ibatis.session.SqlSession;
-
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class test {
 
+    public static void main(String args[]) {
+        Connection c = null;
 
-    public void insertarDatos () throws IOException {
+        Statement stmt = null;
+        String sql = null;
 
-        SqlSession session = BdUtils.getSession();
+        try {
 
-        DrugbankMapper mapper = session.getMapper(DrugbankMapper.class);
-        mapper.selectByPrimaryKey("");
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/test001", "interacciones", "123456");
+            System.out.println("Base de datos abierta correctamente");
 
+            c.setAutoCommit(false);
+            System.out.println("AutoCommit(false)");
 
-        BdUtils.closeSession();
+            stmt = c.createStatement();
+
+            sql = "TRUNCATE TABLE drugbank;";
+            stmt.executeUpdate(sql);
+            System.out.println("TRUNCATE TABLE drugbank;");
+
+            sql = "DROP SEQUENCE seq_drugbank;";
+            stmt.executeUpdate(sql);
+            System.out.println("DROP SEQUENCE seq_drugbank;");
+
+            sql = "CREATE SEQUENCE seq_drugbank start 1 increment 1;";
+            stmt.executeUpdate(sql);
+            System.out.println("CREATE SEQUENCE seq_drugbank start 1 increment 1;");
+
+            sql = "INSERT INTO drugbank (id, drugbank_id, atc, interaccion) VALUES (nextval('seq_drugbank'), 'BTD00024', 'B01AE02','DB01048');";
+            stmt.executeUpdate(sql);
+            System.out.println("Insertando...");
+
+            sql = "INSERT INTO drugbank (id, drugbank_id, atc, interaccion) VALUES (nextval('seq_drugbank'), 'BTD00024', 'B01AE02','DB00054');";
+            stmt.executeUpdate(sql);
+            System.out.println("Insertando...");
+
+            sql = "INSERT INTO drugbank (id, drugbank_id, atc, interaccion) VALUES (nextval('seq_drugbank'), 'BTD00024', 'B01AE02','DB06736');";
+            stmt.executeUpdate(sql);
+            System.out.println("Insertando...");
+
+            sql = "INSERT INTO drugbank (id, drugbank_id, atc, interaccion) VALUES (nextval('seq_drugbank'), 'BTD00024', 'B01AE02','DB01418');";
+            stmt.executeUpdate(sql);
+            System.out.println("Insertando...");
+
+            stmt.close();
+            c.commit();
+            System.out.println("commit;");
+            c.close();
+            System.out.println("Base de datos cerrada correctamente");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
 
     }
-
-
 
 }
