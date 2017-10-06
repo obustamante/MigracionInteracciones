362e663ca4fdcead2f8e7e5f5d1206251e419fed
Oscar <oscar.bustamante01@usc.edu.co>
2017-10-06 11:20:26 -0500

    prueba de conexión

--- src/co/edu/usc/vision/interacciones/api/fileRider/ReadFile.java
+++ src/co/edu/usc/vision/interacciones/api/fileRider/ReadFile.java
@@ -1,10 +1,13 @@
 package co.edu.usc.vision.interacciones.api.fileRider;
 
 
+import co.edu.usc.vision.interacciones.api.utiles.BdUtils;
 import co.edu.usc.vision.interacciones.api.utiles.Util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -22,11 +25,17 @@ public class ReadFile {
     }
 
     /**
-     *
      * Abre y lee un archivo.
+     *
      * @param filename
      */
     private void readFile(String filename) {
+
+        cleanDrugbank();
+
+        String drugbank_id = null;
+        String atc_id = null;
+        String drugbank_id_asociado = null;
 
         int totalLineas = 0;
 
@@ -42,7 +51,6 @@ public class ReadFile {
         final Pattern patternInteraccionClose = Pattern.compile(regexInteraccionClose);
         final Pattern patternDrugbankId = Pattern.compile(regexDrugbankId);
 
-
         List<String> records = new ArrayList<String>();
 
         try {
@@ -52,22 +60,25 @@ public class ReadFile {
 
                 final Matcher matcherId = patternDrugId.matcher(line);
 
+                drugbank_id = null;
+                atc_id = null;
+
                 //Lectura de drugbank-id primary
-                while (matcherId.find()){
+                while (matcherId.find()) {
                     line = reader.readLine();
-                    //System.out.println(line);
-                    System.out.println(line.substring(15,23));
+                    drugbank_id = line.substring(15, 23);
+                    System.out.println(drugbank_id + " - id-drugbank");
                     totalLineas += 1;
                     break;
                 }
-
 
                 final Matcher matcherACT = patternATC.matcher(line);
 
                 // lectura del ATC
                 while (matcherACT.find()) {
                     line = reader.readLine();
-                    System.out.println(line);
+                    atc_id = line.substring(20, 27);
+                    System.out.println("    " + atc_id + " - id-atc");
                     totalLineas += 1;
                     break;
                 }
@@ -77,16 +88,24 @@ public class ReadFile {
 
                 boolean interaccionesOpen = matcherInteraccionOpen.find();
 
+                drugbank_id_asociado = null;
+
                 while (interaccionesOpen) {
                     //Lectura de las interacciones asociadas
                     line = reader.readLine();
                     totalLineas += 1;
 
-
                     final Matcher matcherDrugbankId = patternDrugbankId.matcher(line);
                     // imprime el Drugbank-Id asociado si es encontrado
-                    if(matcherDrugbankId.find()){
-                        System.out.println(line);
+                    if (matcherDrugbankId.find()) {
+
+                        drugbank_id_asociado = line.substring(20, 26);
+                        System.out.println("        " + drugbank_id_asociado + " - Drugbank-Id asociado");
+
+                        // insertando en la tabla Drugbank
+                        insertDrugbank(drugbank_id, atc_id, drugbank_id_asociado );
+
+
                     }
 
                     //sale de las interacciones
@@ -118,6 +137,70 @@ public class ReadFile {
 
     }
 
+    /**
+     * Prepara la tabla Drugbank para insertar los nuevos datos
+     */
+    private void cleanDrugbank() {
+
+        Statement stmt = null;
+
+        Connection c = BdUtils.getSession();
+
+        try {
+
+            stmt = c.createStatement();
+
+            stmt.executeUpdate("TRUNCATE TABLE drugbank;");
+            System.out.println("TRUNCATE TABLE drugbank;");
+
+            stmt.executeUpdate("DROP SEQUENCE seq_drugbank;");
+            System.out.println("DROP SEQUENCE seq_drugbank;");
+
+            stmt.executeUpdate("CREATE SEQUENCE seq_drugbank start 1 increment 1;");
+            System.out.println("CREATE SEQUENCE seq_drugbank start 1 increment 1;");
+
+            stmt.close();
+            c.commit();
+            System.out.println("commit;");
+            BdUtils.closeSession();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+
+    /**
+     * Insertar datos en la tabla Drugbank
+     * @param drugbank
+     * @param atc
+     * @param asociado
+     */
+    private void insertDrugbank(String drugbank, String atc, String asociado) {
+        Statement stmt = null;
+
+        Connection c = BdUtils.getSession();
+
+        try {
+
+            stmt = c.createStatement();
+
+            stmt.executeUpdate("INSERT INTO drugbank (id, drugbank_id, atc, interaccion) VALUES (nextval('seq_drugbank'), " + drugbank + "," + atc + "," + asociado + ");");
+            System.out.println("Insertando...");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+
+    }
 
     public static void main(String[] args) {
 
